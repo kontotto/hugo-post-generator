ac088628cbad15238206064af4e587a0ab16b271
Shuta Kondo <takekon591@gmail.com>
2018-11-24 06:16:51 +0000

    fix test

--- main.go
+++ main.go
@@ -1,71 +1,38 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"log"
 	"os"
 	"text/template"
 )
 
-type Metadata struct {
-	Categories  []string `json:"categories"`
-	Description string   `json:"description"`
-	Tags        []string `json:"tags"`
-	Url         string   `json:"url"`
-}
-
 type Template struct {
-	Categories  []string
-	Date        string
-	Description string
-	Movie       string
-	Tags        []string
-	Thumbnail   string
+	Category  string
+	Date      string
+	Embed     string
+	Thumbnail string
+	Title     string
 }
 
-func BuildTemplate(m *Metadata) (*Template, error) {
+func BuildTemplate() (*Template, error) {
 	t := &Template{
-		Categories:  m.Categories,
-		Date:        "",
-		Description: m.Description,
-		Movie:       "",
-		Tags:        m.Tags,
-		Thumbnail:   "",
+		Category:  "",
+		Date:      "",
+		Embed:     "",
+		Thumbnail: "",
+		Title:     "",
 	}
 	return t, nil
 }
 
-func OpenTemplate(path string) (*template.Template, error) {
-	t := template.Must(template.ParseFiles(path))
-	return t, nil
-}
-
-func OpenMetadata(path string) (*Metadata, error) {
-	bytes, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	metadata := &Metadata{}
-	if err := json.Unmarshal(bytes, &metadata); err != nil {
-		return nil, err
-	}
-	return metadata, nil
+func OpenTemplate(path string) *template.Template {
+	return template.Must(template.ParseFiles(path))
 }
 
 func main() {
-	tmpl, err := OpenTemplate("tests/testdata/templates/movie.tmpl")
-	if err != nil {
-		log.Fatal(err)
-	}
+	tmpl := OpenTemplate("tests/movies/20100101/sm12345678.md.tmpl")
 
-	m, err := OpenMetadata("tests/testdata/movies/20181120/metadata.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	t, err := BuildTemplate(m)
+	t, err := BuildTemplate()
 	if err != nil {
 		log.Fatal(err)
 	}
--- main_test.go
+++ main_test.go
@@ -4,23 +4,13 @@ import (
 	"testing"
 )
 
-func TestOpenMetadataSuccess(t *testing.T) {
-	_, err := OpenMetadata("tests/testdata/movies/20181120/metadata.json")
-	if err != nil {
-		t.Fatal("failed test")
-	}
-}
-
-func TestOpenMetadataFailed(t *testing.T) {
-	_, err := OpenMetadata("unknown.json")
-	if err == nil {
-		t.Fatal("failed test")
-	}
-}
-
 func TestOpenTemplateSuccess(t *testing.T) {
-	_, err := OpenTemplate("tests/testdata/templates/movie.tmpl")
-	if err != nil {
-		t.Fatal("failed test")
+	tmpl := OpenTemplate("tests/movies/20100101/sm12345678.md.tmpl")
+
+	expected := "sm12345678.md.tmpl"
+	actual := tmpl.Name()
+
+	if expected != actual {
+		t.Fail()
 	}
 }
